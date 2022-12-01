65499795e0ec27d8e404656b0f4dcd6a5528e995
TaskMasterErnest <ernestklu08@gmail.com>
2022-12-01 09:13:39 +0000

    updated code for ch_date

--- normalize_date/ch_date.sh
+++ normalize_date/ch_date.sh
@@ -13,7 +13,7 @@ monthNumToName()
     7 ) month="Jul" ;;  8 ) month="Aug" ;;
     9 ) month="Sep" ;;  10) month="Oct" ;;
     11) month="Nov" ;;  12) month="Dec" ;;
-    * ) echo "$0: Unknown month value" >&2
+    * ) echo "$0: Unknown month value $1" >&2
         exit 1
     esac
     return 0
@@ -32,13 +32,14 @@ if [ $3 -le 999 ]; then
 fi
 
 # this next piece checks if the month field contains a number and changes it accordingly.
-# is the mot=nth inout format a number?
+# is the month input format a number?
 if [ -z$(echo $1 |sed 's/[[:digit:]]//g') ]; then
     monthNumToName $1
 else
+# if the month input is a string
 # normalize the first 3 letters, first upper-, then lowercase.
-    month="$(echo $1 | cut -c1 | tr '[:lower:]' '[:upper:]')"
-    month="$month$(echo $1|cut -c2-3|tr '[:upper:]' '[:lower:]')"
+    month="$(echo $1|cut -c1|tr '[:lower:]' '[:upper:]')"
+    month="$month$(echo $1|cut -c2-3 | tr '[:upper:]' '[:lower:]')"
 fi
 
 echo $month $2 $3
